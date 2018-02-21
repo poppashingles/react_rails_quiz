c960368ff34160d7ce7b4ccb1a2a111d35b12a6b
Rob <robdwilliams85@gmail.com>
2018-02-21 12:32:00 +0000

    API data is displaying

--- quiz-api/config/routes.rb
+++ quiz-api/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :questions
+        resources :answers  
+      end
+    end
+  end
+
 end
--- quiz-api/db/schema.rb
+++ quiz-api/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221115131) do
+ActiveRecord::Schema.define(version: 20180221120154) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "answer"
+    t.integer "points"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "question"
