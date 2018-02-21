7f48e7d84a4fedcb3344911c52d56de1b8029eb3
Rob <robdwilliams85@gmail.com>
2018-02-21 15:51:23 +0000

    Returning questions and answers as data

--- quiz/src/components/Answers.js
+++ quiz/src/components/Answers.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Answers extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      answers: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/answers.json')
+    .then(response => {
+      console.log(response)
+      this.setState({answers: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
--- quiz/src/components/QuestionContainer.js
+++ quiz/src/components/QuestionContainer.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import Answers from './Answers'
+import axios from 'axios'
 
 class QuestionContainer extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      question: '',
+      questions: [],
       answers: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/questions.json')
+    .then(response => {
+      console.log(response)
+      this.setState({questions: response.data})
+    })
+    .catch(error => console.log(error))
   }
 
   render() {
