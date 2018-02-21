7ba06e55d70cdc21d9ca00fa8459971ea50786fb
Rob <robdwilliams85@gmail.com>
2018-02-21 15:20:18 +0000

    Initial home page setup

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuestionContainer from './components/QuestionContainer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Welcome to the Cash Flow Quiz!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <QuestionContainer />
       </div>
     );
   }
---
+++ quiz/src/components/QuestionContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class QuestionContainer extends Component {
+  render() {
+    return (
+      <div>
+        Question
+      </div>
+    )
+  }
+}
+
+export default QuestionContainer
