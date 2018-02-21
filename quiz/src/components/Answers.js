eda69c72ba15d30fc4c25cd0a08ae62b66b40f89
Rob <robdwilliams85@gmail.com>
2018-02-21 15:28:02 +0000

    Added question and answer components and hooked them up to app.js

---
+++ quiz/src/components/Answers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Answers extends Component {
+  render() {
+    return (
+      <div>
+        Answers
+      </div>
+    )
+  }
+}
+
+export default Answers
--- quiz/src/components/QuestionContainer.js
+++ quiz/src/components/QuestionContainer.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Answers from './Answers'
 
 class QuestionContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      question: '',
+      answers: []
+    }
+  }
+
   render() {
     return (
       <div>
         Question
+        <Answers />
       </div>
     )
   }
